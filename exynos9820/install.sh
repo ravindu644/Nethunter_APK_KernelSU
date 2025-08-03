04bccc9320940cd3ce53d0e77f4354774f1be98d
ravindu644 <droidcasts@protonmail.com>
2025-08-03 12:33:18 +0530

    Placing the rc file using a magisk module does not work !

--- exynos9820/install.sh
+++ exynos9820/install.sh
@@ -43,6 +43,21 @@ check_device() {
     abort
   fi
 
+USB_RC="/vendor/etc/init/init.$(getprop ro.hardware).usb.rc"
+
+if grep -q "nethunter" "$USB_RC"; then
+    ui_print "[i] Custom init.$(getprop ro.hardware).usb.rc file found. Proceeding with installation..."
+else
+    ui_print "[-] This specific script will only work with ravindu644's custom init.$(getprop ro.hardware).usb.rc file."
+    sleep 1
+    ui_print "Opening browser to download the required file..."
+    sleep 1
+    ui_print "Download it and place it in /vendor/etc/init/ directory."
+    sleep 1
+    nohup am start -a android.intent.action.VIEW -d https://raw.githubusercontent.com/ravindu644/Nethunter_APK_KernelSU/exynos9820-nethunter/exynos9820/vendor/etc/init/init.exynos9820.usb.rc >/dev/null 2>&1 &
+    abort
+fi
+
   ui_print "Device & Kernel check passed."
   ui_print " "
 }
