16dd136414cfe6b9716f58ef164350db31473879
ravindu644 <droidcasts@protonmail.com>
2025-08-05 23:16:24 +0530

    cleanup

--- exynos9820/module/META-INF/com/google/android/util_functions.sh
+++ exynos9820/module/META-INF/com/google/android/util_functions.sh
@@ -9,6 +9,36 @@ export KERNEL_VERSION=$(uname -r)
 export KERNEL_PATH="$TMPDIR/kernel/$PRODUCT_NAME"
 export BOOT_BACKUP_DIR="/sdcard/BootBackup"
 
+check_product() {
+  hw=$(getprop ro.hardware)
+  kernel=$(uname -r)
+
+  ui_print "Verifying device..."
+  ui_print " "
+
+  # Check hardware first
+  case "$hw" in
+    exynos9820) ;;
+    *)
+      ui_print "Unsupported device: $hw"
+      ui_print "This module is only for exynos9820 devices."
+      abort
+      ;;
+  esac
+
+  # Check if kernel string contains nh/nethunter
+  ui_print "Detected Kernel version: $kernel"
+  echo "$kernel" | grep -iqE "4.14.113"
+  if [ $? -ne 0 ]; then
+    ui_print "Unsupported Kernel version: $kernel"
+    ui_print "This module requires a 4.14.113-xxxxx kernel."    
+    abort "Please install the stock boot.img or a stable 4.14.113 kernel for your device and re-flash this module."
+  fi
+  ui_print " "
+  ui_print "Device & Kernel check passed."
+  ui_print " "
+}
+
 check_device() {
     
     if [ "$ANDROID_VERSION" != "12" ]; then
@@ -71,6 +101,19 @@ install_kernel(){
     return 0
 }
 
+prepare_nethunter_kernel(){ 
+
+  ui_print "Checking prebuilt kernel..."
+  unzip -o "$ZIPFILE" 'kernel' -d $TMPDIR >&2
+
+  if install_kernel; then
+      KERNEL_FLASH_SUCCESS="1"
+  else
+      abort "No Nethunter kernel found for device $PRODUCT_NAME, aborting..."
+  fi  
+    
+}
+
 warning(){
     ui_print "******************************************"
     ui_print "*        WARNING WARNING WARNING         *"
@@ -82,4 +125,64 @@ warning(){
     ui_print "*           or ODIN mode                 *"
     ui_print "******************************************"
     ui_print " "
+}
+
+install_files(){
+
+  ui_print "Installing files..."
+
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+
+
+}
+
+install_app(){
+
+  ui_print "Uninstalling original Nethunter app..."
+  pm uninstall com.offsec.nethunter &>/dev/null || true
+
+  ui_print "Installing the new Nethunter app..."
+
+  unzip -o "$ZIPFILE" 'data/*' -d $TMPDIR >&2
+  pm install -r $TMPDIR/data/NetHunter_exynos9820_signed.apk &>/dev/null || abort "Failed to install the Nethunter app...!"
+
+  ui_print "Granting required permissions..."
+
+  for x in ACCESS_BACKGROUND_LOCATION \
+          ACCESS_COARSE_LOCATION \
+          ACCESS_FINE_LOCATION \
+          READ_EXTERNAL_STORAGE \
+          WRITE_EXTERNAL_STORAGE \
+          WRITE_SECURE_SETTINGS; do
+    pm grant -g com.offsec.nethunter android.permission.$x 2>/dev/null && \
+      ui_print "** $x success" || ui_print "** ! $x failed"
+  done
+
+}
+
+boot_scripts(){
+
+  ui_print "Copying boot scripts..."
+
+  (unzip -o "$ZIPFILE" 'post-fs-data.sh' -d $MODPATH >&2
+  unzip -o "$ZIPFILE" 'service.sh' -d $MODPATH >&2) || abort "Failed to copy boot scripts..."
+
+}
+
+cleanup_nh(){
+
+rm -rf /data/system/package_cache/*
+
+ui_print " "
+
+}
+
+success_nh(){
+
+  ui_print " "
+  ui_print "Nethunter Kernel with full USB Arsenal support for exynos9820 devices is installed successfully!"
+  ui_print " "    
+  ui_print "Please reboot your device to apply changes."
+  ui_print " "  
+
 }
