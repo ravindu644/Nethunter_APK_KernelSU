bfb390fee716cbfec1ee1f83389685755e7972dc
ravindu644 <droidcasts@protonmail.com>
2025-08-06 01:34:52 +0530

    bump version 1.5-beta
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- exynos9820/module/uninstall.sh
+++ exynos9820/module/uninstall.sh
@@ -1,3 +1,9 @@
+if [  -f /sdcard/BootBackup/boot.img ]; then
+    ui_print "Backup of the original boot.img found, restoring..."
+    dd if=/sdcard/BootBackup/boot.img of=/dev/block/by-name/boot
+fi
 
-  rm -rf /data/system/package_cache/*
-  
+if [  -f /sdcard/BootBackup/dtb.img ]; then
+    ui_print "Backup of the original dtb.img found, restoring..."
+    dd if=/sdcard/BootBackup/dtb.img of=/dev/block/by-name/dtb
+fi
