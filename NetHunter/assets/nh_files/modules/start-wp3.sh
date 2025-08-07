0896398885d9651e2ece54611df1b9b032068e79
ravindu644 <droidcasts@protonmail.com>
2025-08-08 01:01:11 +0530

    fixed an issue where the AP has quotes like \'this\'
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- NetHunter/assets/nh_files/modules/start-wp3.sh
+++ NetHunter/assets/nh_files/modules/start-wp3.sh
@@ -6,6 +6,10 @@ BSSID=00:11:22:33:44:55
 CHANNEL=1
 TEMPLATE=""
 WLAN0TO1=1
+
+# Strip any quotes from SSID variable (in case they were accidentally included)
+STRIPPED_SSID=$(echo "$SSID" | sed 's/^"//;s/"$//' | sed "s/^'//;s/'$//")
+
 command -v wifipumpkin3 >/dev/null 2>&1 || { echo 'wifipumpkin3 is missing, installing..'; apt update && apt install wifipumpkin3 -y; }
 command -v dnschef >/dev/null 2>&1 || { echo 'dnschef is missing, installing..'; apt update && apt install dnschef -y; }
 echo "Checking if config folder exists.."
@@ -27,7 +31,6 @@ DEF=$(ip route show table $table|grep default|grep $NETIFACE)
   fi
 done
 echo "Default rule number is $table"
-
 
 echo "Checking for existing $APIFACE interface.."
 if ip link show $APIFACE; then
@@ -63,20 +66,22 @@ ip rule add from all iif lo oif $APIFACE uidrange 0-0 lookup 97 pref 11000 2> /d
 ip rule add from all iif lo oif $NETIFACE lookup $table pref 17000 2> /dev/null
 ip rule add from all iif lo oif $APIFACE lookup 97 pref 17000 2> /dev/null
 ip rule add from all iif $APIFACE lookup $table pref 21000 2> /dev/null
+
 echo "Starting wifipumpkin3 and dnschef.."
 if [[ ! $TEMPLATE == "" ]]; then
   TemplateCMD=" set captiveflask.$TEMPLATE true;"
   CaptiveCMD=" set proxy captiveflask true;"
   else CaptiveCMD=" set proxy noproxy;"
 fi
-#echo $SSID
-#echo $CaptiveCMD
-#echo $TemplateCMD
+
+# Debug output to verify SSID value
+echo "DEBUG: Original SSID: [$SSID]"
+echo "DEBUG: Stripped SSID will be used: [$STRIPPED_SSID]"
+
 sleep 20 && dnschef --interface 10.0.0.1 &
 
-# ------------------- FIXED COMMAND BELOW -------------------
-wifipumpkin3 --xpulp "set interface $APIFACE; set interface_net $NETIFACE; set ssid '$SSID'; set bssid $BSSID; set channel $CHANNEL;${CaptiveCMD}${TemplateCMD} start; ap"
-# -----------------------------------------------------------
+# Fixed wifipumpkin3 command - using STRIPPED_SSID without quotes
+wifipumpkin3 --xpulp "set interface $APIFACE; set interface_net $NETIFACE; set ssid $STRIPPED_SSID; set bssid $BSSID; set channel $CHANNEL;${CaptiveCMD}${TemplateCMD} start; ap"
 
 pkill python3
 echo "Restoring iptables rules.."
